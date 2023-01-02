21a1bc9bfa180b0e4499dca64fdb47e9360f7f43
Lukas Mazan <115067813+Leg3ndYe@users.noreply.github.com>
2023-01-02 14:28:12 +0100

    Add files via upload

---
+++ C# Learning/Functions/Functions/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5;           DoubleIt
+            //int solution;
+            //DoubleIt(x, out solution);
+            //Console.WriteLine("15 * 2 = {0}", solution);
+
+
+            //double x = 4;        GetSum
+            //double y = 5;
+            //Console.WriteLine(GetSum(x, y));
+
+
+            //int num3 = 10;       Swap
+            //int num4 = 20;
+            //Console.WriteLine("Before Swap num1 : {0} num2 : {1}", num3 , num4);
+            //Swap(ref num3, ref num4);
+            //Console.WriteLine("After Swap num1 : {0} num2 : {1}", num3, num4);
+
+
+            //Console.WriteLine("1 + 2 + 3 = {0}",GetSumMore(1, 2, 3));       GetSumMore
+
+
+            //PrintInfo(zipCode: 15147, name: "DerekBanas");                  PrintInfo
+
+
+            //Console.WriteLine("5.0 + 4.5 = {0}", GetSum2(5.0, 4.5));        GetSum2
+            //Console.WriteLine("5.0 + 4.5 = {0}", GetSum2("5.0", "4.5"));
+
+        }
+        static double GetSum2(double x = 1, double y = 1)
+        {
+            return x + y;
+        }
+        static double GetSum2(string x = "1", string y = "1")
+        {
+            double xblX = Convert.ToDouble(x);
+            double xblY = Convert.ToDouble(x);
+            return xblX + xblY;
+        }
+        static void PrintInfo (string name, int zipCode)
+        {
+            Console.WriteLine("{0} lives in the zip code {1}", name, zipCode);
+        }
+        public static double GetSumMore(params double[] nums)
+        {
+            double sum = 0;
+            foreach (int i in nums) 
+            {
+                sum += i;   
+            }
+            return sum;
+        }
+        public static void Swap (ref int num3, ref int num4)
+        {
+            int temp = num3;
+            num3 = num4;
+            num4 = temp;
+        }
+        static double GetSum (double x, double y)
+        {
+            return x + y;
+        }
+        
+        static void DoubleIt(int x, out int solution)
+        {
+            solution = x * 2;
+        }
+    }
+}
---
+++ C# Learning/GenericsDelegates/GenericsDelegates/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalList = new List<Animal>();
+
+            List<int> numList = new List<int>();
+            numList.Add(24);
+
+            animalList.Add(new Animal() { Name = "Doug" });
+            animalList.Add(new Animal() { Name = "Paul" });
+            animalList.Add(new Animal() { Name = "Sally" });
+
+            animalList.Insert(1, new Animal() { Name = "Steve" });
+            animalList.RemoveAt(1);
+            Console.WriteLine("Num of Animals : {0}", animalList.Count);
+
+            foreach(Animal a in animalList) { Console.WriteLine(a.Name); }
+
+            int x = 5, y = 4;
+            Animal.GetSum<int>(ref x, ref y);
+
+            string strX = "5", strY = "4";
+            Animal.GetSum<string>(ref strX, ref strY);
+
+            Rectangle<int> rec1 = new Rectangle<int>(20, 50);
+            Console.WriteLine(rec1.GetArea());
+
+            Rectangle<string> rec2 = new Rectangle<string>("20", "50");
+            Console.WriteLine(rec2.GetArea());
+
+            Arithmethic add, sub, addSub;
+            add = new Arithmethic(Add);
+            sub = new Arithmethic(Subract);
+            addSub = add + sub;
+
+            Console.WriteLine($"Add {6} & {10}");
+            add(6, 10);
+            Console.WriteLine($"Add & Subtract {10} & {4}");
+            addSub(10, 4);
+        }
+
+        public struct Rectangle<T>
+        {
+            private T width;
+            private T lenght;
+
+            public T Width
+            {
+                get { return width; }
+                set { width = value; }
+            }
+            public T Lenght
+            {
+                get { return lenght; }
+                set { lenght = value; }
+            }
+            public Rectangle(T w, T l)
+            {
+                width = w;
+                lenght = l;
+            }
+            public string GetArea()
+            {
+                double dblW = Convert.ToDouble(Width);
+                double dblL = Convert.ToDouble(Lenght);
+                return string.Format($"{Width} * {Lenght} = {dblW * dblL}");
+            }
+
+        }
+
+        public delegate void Arithmethic(double num1, double num2);
+        public static void Add(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+        public static void Subract(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        }
+    }
+}
