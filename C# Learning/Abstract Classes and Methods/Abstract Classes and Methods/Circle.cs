216dd95c22eb71766170b310fe1ca1b03eb2d018
Lukas Mazan <115067813+Leg3ndYe@users.noreply.github.com>
2023-01-02 14:17:44 +0100

    Add files via upload

---
+++ C# Learning/Abstract Classes and Methods/Abstract Classes and Methods/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_and_Methods
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * (Math.Pow(Radius, 2.0));
+        }
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"It has a Radius of {Radius}");
+        }
+    }
+}
---
+++ C# Learning/Abstract Classes and Methods/Abstract Classes and Methods/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_and_Methods
+{
+    class Rectangle : Shape
+    {
+        public double Lenght { get; set; }
+        public double Width { get; set; }
+        public Rectangle(double lenght, double width)
+        {
+            Name = "Rectangle";
+            Lenght = lenght;
+            Width = width;
+        }
+        public override double Area()
+        {
+            return Lenght * Width;
+        }
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"It has a Lenght of {Lenght} and Width of {Width}");
+        }
+    }
+}
